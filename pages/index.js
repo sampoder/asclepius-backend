c1825377124b0a5e7fad31be2ea308aa93860050
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-11-10 18:55:32 +0800

    Clean up auth

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
-import { useUser } from '../lib/hooks'
-import { useRouter } from 'next/router'
+import { useUser } from "../lib/hooks";
+import { useRouter } from "next/router";
+import { Container, Center } from "@chakra-ui/core";
 
 const Home = () => {
-  const user = useUser()
-  const router = useRouter()
-  if (!user && process.browser){
-    router.push('/login')
+  const user = useUser();
+  const router = useRouter();
+  if (!user && process.browser) {
+    router.push("/login");
   }
   return (
     <>
       {user && (
-        <>
-          <p>Currently logged in as:</p>
-          <pre>{JSON.stringify(user, null, 2)}</pre>
-        </>
+        <Center h="100vh">
+          <Container maxW="xl">
+            <p>Currently logged in as:</p>
+            <pre>{JSON.stringify(user, null, 2)}</pre>
+          </Container>
+        </Center>
       )}
 
       <style jsx>{`
@@ -22,7 +25,7 @@ const Home = () => {
         }
       `}</style>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
