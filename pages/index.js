8c08658ef67d687a30d6c532783450a86bcabdf5
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-11-10 20:41:34 +0800

    Clean up UI :sparkles:

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,26 @@
 import { useUser } from "../lib/hooks";
 import { useRouter } from "next/router";
-import { Container, Center, Heading, Text } from "@chakra-ui/core";
+import {
+  Container,
+  Center,
+  Code,
+  Heading,
+  Text,
+  Box,
+  Button,
+  Image,
+  Link,
+  Badge,
+  Stat,
+  Flex,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+  Grid,
+  GridItem,
+} from "@chakra-ui/core";
 
 const Home = () => {
   const user = useUser();
@@ -11,13 +31,62 @@ const Home = () => {
   return (
     <>
       {user && (
-        <Center h="100vh">
-          <Container maxW="xl">
-            <Heading pb={6}>👋 Welcome Nodemaster</Heading>
-            <Text pb={9}>Currently logged in as:</Text>
-            <Text pt={9}><pre>{JSON.stringify(user, null, 2)}</pre></Text>
-          </Container>
-        </Center>
+        <Container maxW="xl" pt={100}>
+          <Heading pb={6}>👋 Welcome Nodemaster</Heading>
+          <Text pb={9}>
+            You are currently logged in as:{" "}
+            <Code style={{ wordWrap: "break-word" }} p={1} borderRadius={8}>
+              {user.email}
+            </Code>
+          </Text>
+          <hr style={{ paddingBottom: "10px", paddingTop: "10px" }} />
+          <Flex>
+            <Box maxW="sm" borderWidth="1px" rounded="lg" overflow="hidden" mr={10}>
+              <Image
+                objectFit="cover"
+                w="100%"
+                src={
+                  "https://images.unsplash.com/photo-1582389881120-ea93ec8c1d10?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+                }
+                alt={"Postal Box"}
+              />
+              <Box p="6">
+                <Box
+                  mt="1"
+                  fontWeight="semibold"
+                  as="h4"
+                  lineHeight="tight"
+                  isTruncated
+                >
+                  Send Packages
+                </Box>
+              </Box>
+            </Box>
+            <Box maxW="sm" borderWidth="1px" rounded="lg" overflow="hidden">
+              <Image
+                objectFit="cover"
+                w="100%"
+                src={
+                  "https://images.unsplash.com/photo-1556011308-d6aedab5ed8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+                }
+                alt={"DHL Truck"}
+              />
+              <Box p="6">
+                <Box
+                  mt="1"
+                  fontWeight="semibold"
+                  as="h4"
+                  lineHeight="tight"
+                  isTruncated
+                >
+                  Track Packages
+                </Box>
+              </Box>
+            </Box>
+          </Flex>
+          <hr style={{ marginBottom: "10px", marginTop: "20px" }} />
+          <Text>© Asclepius 2020{'⠀'}|{'⠀'}<Link href="https://github.com/sampoder/asclepius-backend" color="blue.500">Source</Link>{'⠀'}|{'⠀'}Images from <Link href="https://unsplash.com" color="blue.500">Unsplash</Link></Text>
+        </Container>
       )}
 
       <style jsx>{`
@@ -25,7 +94,7 @@ const Home = () => {
           margin-bottom: 0.5rem;
         }
         pre {
-          background: #EDF2F7;
+          background: #edf2f7;
           padding: 15px;
           border-radius: 10px;
         }
